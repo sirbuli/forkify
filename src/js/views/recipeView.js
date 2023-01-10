759b4ce9d22ce38b49603f40bd73a30db93c87c1
sirbuli <bulibulislaw@gmail.com>
2023-01-10 20:52:30 +0100

    Change fractional to fractory

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
